10d90d8d1e321c310b97ab577bc0852bca2e96a5
Nicolas <nicolas.rfontes@gmail.com>
2014-08-21 14:55:15 -0300

    First

---
+++ src/main/java/br/leona/hardware/model/Servico.java
@@ -0,0 +1,29 @@
+package br.leona.hardware.model;
+
+/**
+ *
+ * @author Jean Negrão
+ */
+public class Servico {
+    
+    private String nome;
+    private String status;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+}
---
+++ src/main/java/br/leona/hardware/service/RecuperadorServico.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.leona.hardware.service;
+
+import br.leona.hardware.model.Servico;
+
+/**
+ *
+ * @author Jean Negrão
+ */
+public interface RecuperadorServico {   
+     
+    abstract Servico getServico();
+       
+}
